64f47fd2eacb275a9b0f78ec12e5a6f1f1481cda
summerine <indri.pr.26@gmail.com>
2019-02-11 11:58:16 +0700

    fixing tambah pegawai

--- includes/tambah_pegawai.php
+++ includes/tambah_pegawai.php
@@ -5,9 +5,9 @@ require "../config/config.php";
 
 if (isset($_POST['save'])) {
     $user = mysqli_real_escape_string($connection, $_POST['user']);
-    $namaSlack = mysqli_real_escape_string($connection, $_POST['namaSlack']);
+    $name = mysqli_real_escape_string($connection, $_POST['name']);
 
-    $sql = mysqli_query($connection, "INSERT INTO tb_pegawai (id, user, namaSlack) VALUES ('', '$user', '$namaSlack')");  
+    $sql = mysqli_query($connection, "INSERT INTO tb_pegawai (id, user, name) VALUES ('', '$user', '$name')");  
 
 if ($sql){
 	echo "<script>window.location='tabel_pegawai.php'</script>";
